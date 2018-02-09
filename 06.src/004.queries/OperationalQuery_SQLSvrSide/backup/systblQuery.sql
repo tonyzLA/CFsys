a6960d5bd789548047a4640d6b454ab7a12bf715
tonyjm202 <tonyztzhu@hotmail.com>
2018-02-08 16:50:55 -0800

    update scripts
    Tony @2/8/2018

--- 06.src/004.queries/OperationalQuery_SQLSvrSide/backup/systblQuery.sql
+++
@@ -1,7 +0,0 @@
-select objs.name, cols.name  from sys.columns AS cols JOIN sys.objects objs ON cols.object_id = objs.object_id
-where cols.name = 'dimsProfileId'
-
-select objs.name, cols.name  from sys.columns AS cols JOIN sys.objects objs ON cols.object_id = objs.object_id
-where cols.name like '%memo%'
-
-select * from sys.objects
